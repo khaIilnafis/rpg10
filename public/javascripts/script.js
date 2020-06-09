f2f595703059bb2455bed9b72dfbb05818dbcd69
inafis <khalilnafis@gmail.com>
2020-06-09 16:55:50 -0400

    fix

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
             let row = $("<tr>");
             if(player.data){
                 let playerData = JSON.parse(player.data);
-            let rowData = $(`<td>${player.id}</td><td>${player.name}</td><td>${playerData.br.wins}</td>
-            <td>${playerData.br.kills}</td><td>${playerData.br.kdRatio}</td> <td>${playerData.br.downs}</td> <td>${playerData.br.gamesPlayed}</td>
-            <td>${playerData.br.topTen}</td><td>${playerData.br.topFive}</td>`);
+                let rowData = $(`<td>${player.id}</td><td>${player.name}</td><td>${playerData.br.wins}</td>
+                <td>${playerData.br.kills}</td><td>${playerData.br.kdRatio}</td> <td>${playerData.br.downs}</td> <td>${playerData.br.gamesPlayed}</td>
+                <td>${playerData.br.topTen}</td><td>${playerData.br.topFive}</td>`);
+                row.append(rowData);
             }
-            row.append(rowData);
             $('tbody').append(row);
         })
     })
