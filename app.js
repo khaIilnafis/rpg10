f244e8740bf475bbc36d548554023141b5a7a945
inafis <khalilnafis@gmail.com>
2020-06-09 01:48:25 -0400

    static fix

--- app.js
+++ app.js
@@ -15,6 +15,34 @@ const API = require('call-of-duty-api')({platform: "xbl"});
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 var theboys = ["WEBSTARIO1","xNINJA MOx","MrSheeshh","KKS","allanlallana","Nj x Pharaoh", "IIXLord0fWarXII","Barbarossa3","IIxxDEICIDExxII","ry7393","thespaceghostwm","XvXTsukuyomiXvX",
 "STEPHBRYANT23","JankyJamaican","XXIHooliganIXX","icepick1213","Mr X727","masizzai","cojomatic","iSnipe Hot Java"];
 
@@ -60,33 +88,4 @@ function grabStats(theboy){
         console.log(err);
     })
 }
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
