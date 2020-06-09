94029af7196386bb102857403996b39b41ea9d72
inafis <khalilnafis@gmail.com>
2020-06-09 08:31:09 -0400

    remove wins

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
     $.get('/all-players')
     .then(function(players){
-        console.log(players)
-        let lastUpdated = moment(players[0].updatedAt).format('MMMM Do YYYY, h:mm:ss a');
+        let lastUpdated = moment(players[0].updatedAt).format('MMMM Do YYYY, hh:mm:ss a');
         $("#header").append(`<span> Last Updated: ${lastUpdated}`);
         players.forEach(player => {
             let row = $("<tr>");
             let playerData = JSON.parse(player.data);
-            let rowData = $(`<td>${player.id}</td><td>${player.name}</td><td>Wins: ${playerData.br.wins}</td>
+            let rowData = $(`<td>${player.id}</td><td>${player.name}</td><td>${playerData.br.wins}</td>
             <td>${playerData.br.kills}</td><td>${playerData.br.kdRatio}</td> <td>${playerData.br.downs}</td> <td>${playerData.br.gamesPlayed}</td>
             <td>${playerData.br.topTen}</td><td>${playerData.br.topFive}</td>`);
             row.append(rowData);
