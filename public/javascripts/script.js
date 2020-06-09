e80e1fdd477e78135838566969dfe6391091f517
inafis <khalilnafis@gmail.com>
2020-06-09 16:53:36 -0400

    error fix

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
         let lastUpdated = moment(players[0].updatedAt).format('MMMM Do YYYY, hh:mm:ss a');
         players.forEach(player => {
             let row = $("<tr>");
-            let playerData = JSON.parse(player.data);
+            if(player.data){
+                let playerData = JSON.parse(player.data);
             let rowData = $(`<td>${player.id}</td><td>${player.name}</td><td>${playerData.br.wins}</td>
             <td>${playerData.br.kills}</td><td>${playerData.br.kdRatio}</td> <td>${playerData.br.downs}</td> <td>${playerData.br.gamesPlayed}</td>
             <td>${playerData.br.topTen}</td><td>${playerData.br.topFive}</td>`);
+            }
             row.append(rowData);
             $('tbody').append(row);
         })
