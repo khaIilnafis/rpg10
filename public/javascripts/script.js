bf74072bcc9a5b1e5d044c5d488b222a52e41964
inafis <khalilnafis@gmail.com>
2020-06-09 16:57:34 -0400

    test

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         let lastUpdated = moment(players[0].updatedAt).format('MMMM Do YYYY, hh:mm:ss a');
         players.forEach(player => {
             let row = $("<tr>");
-            if(player.data){
+            if(player.data.length > 3){
                 let playerData = JSON.parse(player.data);
                 let rowData = $(`<td>${player.id}</td><td>${player.name}</td><td>${playerData.br.wins}</td>
                 <td>${playerData.br.kills}</td><td>${playerData.br.kdRatio}</td> <td>${playerData.br.downs}</td> <td>${playerData.br.gamesPlayed}</td>
